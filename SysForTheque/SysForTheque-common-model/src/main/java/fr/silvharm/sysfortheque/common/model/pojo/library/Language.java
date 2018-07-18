f50f154b1e547e7cd48b30cbc3ca3445fb66e813
SilvHarm <silvharm@gmail.com>
2018-07-18 14:47:57 +0200

    Added LibraryBook related POJOs in common-model

---
+++ SysForTheque/SysForTheque-common-model/src/main/java/fr/silvharm/sysfortheque/common/model/pojo/library/Language.java
@@ -0,0 +1,100 @@
+package fr.silvharm.sysfortheque.common.model.pojo.library;
+
+
+public class Language {
+	
+	private Integer id;
+	private String reference;
+	private int bookId;
+	private int languageId;
+	
+	
+	public Language() {}
+	
+	
+	/**
+	 * Used when a new Language need to be added to the database
+	 * 
+	 * @param reference
+	 * @param bookId
+	 * @param languageId
+	 */
+	public Language(String reference, int bookId, int languageId) {
+		this.reference = reference;
+		this.bookId = bookId;
+		this.languageId = languageId;
+	}
+	
+	
+	/********************************
+	 * Getters & Setters
+	 *******************************/
+	
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	
+	
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	/**
+	 * @return the reference
+	 */
+	public String getReference() {
+		return reference;
+	}
+	
+	
+	/**
+	 * @param reference
+	 *           the reference to set
+	 */
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	
+	
+	/**
+	 * @return the bookId
+	 */
+	public int getBookId() {
+		return bookId;
+	}
+	
+	
+	/**
+	 * @param bookId
+	 *           the bookId to set
+	 */
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	
+	
+	/**
+	 * @return the languageId
+	 */
+	public int getLanguageId() {
+		return languageId;
+	}
+	
+	
+	/**
+	 * @param languageId
+	 *           the languageId to set
+	 */
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+	
+}
---
+++ SysForTheque/SysForTheque-common-model/src/main/java/fr/silvharm/sysfortheque/common/model/pojo/library/LibraryBook.java
@@ -0,0 +1,80 @@
+package fr.silvharm.sysfortheque.common.model.pojo.library;
+
+
+public class LibraryBook {
+	
+	private Integer id;
+	private String language;
+	private String code;
+	
+	
+	public LibraryBook() {}
+	
+	
+	/**
+	 * Used when a new LibraryBook need to be added to the database
+	 * 
+	 * @param language
+	 * @param code
+	 */
+	public LibraryBook(String language, String code) {
+		this.language = language;
+		this.code = code;
+	}
+	
+	
+	/********************************
+	 * Getters & Setters
+	 *******************************/
+	
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	
+	
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+	
+	
+	/**
+	 * @param language
+	 *           the language to set
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+	
+	
+	/**
+	 * @param code
+	 *           the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
