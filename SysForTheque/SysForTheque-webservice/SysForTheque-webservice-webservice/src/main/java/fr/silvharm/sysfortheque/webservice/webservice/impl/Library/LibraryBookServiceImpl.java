f0920efb184b7909892c8effddcb5aa60f4d5e8a
SilvHarm <silvharm@gmail.com>
2018-08-01 14:16:12 +0200

    Added WebService annotations to Impl Class

--- SysForTheque/SysForTheque-webservice/SysForTheque-webservice-webservice/src/main/java/fr/silvharm/sysfortheque/webservice/webservice/impl/Library/LibraryBookServiceImpl.java
+++ SysForTheque/SysForTheque-webservice/SysForTheque-webservice-webservice/src/main/java/fr/silvharm/sysfortheque/webservice/webservice/impl/Library/LibraryBookServiceImpl.java
@@ -2,16 +2,20 @@ package fr.silvharm.sysfortheque.webservice.webservice.impl.Library;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 import fr.silvharm.sysfortheque.common.model.bean.book.BookBean;
 import fr.silvharm.sysfortheque.common.model.bean.library.LibraryBookBean;
 import fr.silvharm.sysfortheque.common.model.pojo.book.Book;
 import fr.silvharm.sysfortheque.common.model.pojo.book.Genre;
 import fr.silvharm.sysfortheque.webservice.webservice.contract.Library.LibraryBookService;
 
-
+@WebService(serviceName = "LibraryBook")
 public class LibraryBookServiceImpl implements LibraryBookService {
 	
 	@Override
+	@WebMethod
 	public List<BookBean> getBookBeanListByIdList(List<Integer> bookIdList) {
 		// TODO Auto-generated method stub
 		return null;
@@ -19,6 +23,7 @@ public class LibraryBookServiceImpl implements LibraryBookService {
 	
 	
 	@Override
+	@WebMethod
 	public List<Book> getBookListByGenreList(List<Genre> bookGenresList) {
 		// TODO Auto-generated method stub
 		return null;
@@ -26,6 +31,7 @@ public class LibraryBookServiceImpl implements LibraryBookService {
 	
 	
 	@Override
+	@WebMethod
 	public List<Book> getBookListByTitle(String title) {
 		// TODO Auto-generated method stub
 		return null;
@@ -33,6 +39,7 @@ public class LibraryBookServiceImpl implements LibraryBookService {
 	
 	
 	@Override
+	@WebMethod
 	public List<LibraryBookBean> getLibraryBookBeanListByIdList(List<Integer> bookIdList) {
 		// TODO Auto-generated method stub
 		return null;
--- SysForTheque/SysForTheque-webservice/SysForTheque-webservice-webservice/src/main/java/fr/silvharm/sysfortheque/webservice/webservice/impl/user/UserServiceImpl.java
+++ SysForTheque/SysForTheque-webservice/SysForTheque-webservice-webservice/src/main/java/fr/silvharm/sysfortheque/webservice/webservice/impl/user/UserServiceImpl.java
@@ -2,19 +2,25 @@ package fr.silvharm.sysfortheque.webservice.webservice.impl.user;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 import fr.silvharm.sysfortheque.common.model.technical.pojo.RequestResult;
 import fr.silvharm.sysfortheque.webservice.webservice.contract.user.UserService;
 
-
+@WebService(serviceName = "User")
 public class UserServiceImpl implements UserService {
-
+	
 	@Override
+	@WebMethod
 	public List<String> getUserEmailListById(List<Integer> userIdList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	
 	@Override
+	@WebMethod
 	public RequestResult verifyUser(String username, String password) {
 		// TODO Auto-generated method stub
 		return null;
